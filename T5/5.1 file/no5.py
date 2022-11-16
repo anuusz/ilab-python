b6b288bdeb1ce4ed7617ac79ad255b89d8a86501
anuusz <stefanuseclesiox@gmail.com>
2022-11-16 11:29:48 +0700

    benerin source code

--- T5/5.1 file/no5.py
+++ T5/5.1 file/no5.py
@@ -1,9 +1,1 @@
-tambah_kota = input('Masukkan nama kota: ')
-
-output_file = open('kota.txt', 'a')
-
-output_file.write(tambah_kota)
-
-output_file.close()
-
-print('Data telah ditambahkan ke file kota.txt.')
+# program membuat file kota.txt
